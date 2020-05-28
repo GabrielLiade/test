6d430df6b68a779eb817c67f1a3f1aa178444ee7
Gabriel Eliade <gabriel.eliade@wanadoo.fr>
2020-05-28 23:00:13 +0200

    Updates

---
+++ precache-manifest.41d338dbd13bdcda6a4cfb879636bd78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335b77cf1cec589561c424f2e5dbc237",
+    "url": "/fortniteShop.git/index.html"
+  },
+  {
+    "revision": "54ce515fdb4e2a691bc2",
+    "url": "/fortniteShop.git/static/css/main.8475aaa4.chunk.css"
+  },
+  {
+    "revision": "2ef2dfae712141fc8987",
+    "url": "/fortniteShop.git/static/js/2.c0830982.chunk.js"
+  },
+  {
+    "revision": "54ce515fdb4e2a691bc2",
+    "url": "/fortniteShop.git/static/js/main.e76eb35e.chunk.js"
+  },
+  {
+    "revision": "3f36f21fc7e3607b7344",
+    "url": "/fortniteShop.git/static/js/runtime~main.23e8fc95.js"
+  }
+]);
